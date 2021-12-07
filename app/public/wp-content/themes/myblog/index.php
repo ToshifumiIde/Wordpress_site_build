e0ee41458d15ef305f54f1b1898a0f83b8a00dbb
井手 俊文 <knocknote.ide@gmail.com>
2021-12-07 17:42:38 +0900

    create function

--- app/public/wp-content/themes/myblog/index.php
+++ app/public/wp-content/themes/myblog/index.php
@@ -25,7 +25,7 @@
 
 <body>
 
-<?php echo date("Y"); ?>
+<!-- <?php echo date("Y"); ?> -->
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
@@ -61,7 +61,7 @@
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
             <h1>
-              Clean Blog <?php echo date("Yn");?>
+              Clean Blog <?php echo date("Y");?>
             </h1>
             <span class="subheading">
               A Blog Theme by Start Bootstrap
@@ -79,16 +79,20 @@
         <div class="post-preview">
           <a href="post.html">
             <h2 class="post-title">
-            <?php the_title() ;?>
+              <?php $title = the_title("☆" , "◯" ,false); ?>
+              <!-- <?php echo $title; ?> -->
+              <?php echo str_replace("新規" , "過去の" , $title);?>
             </h2>
-
             <h3 class="post-subtitle">
               Problems look mighty small from 150 miles up
             </h3>
           </a>
           <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 24, 2019</p>
+            <a href="#">
+              Start Bootstrap
+            </a>
+            on September 24, 2019
+          </p>
         </div>
         <hr>
         <div class="post-preview">
